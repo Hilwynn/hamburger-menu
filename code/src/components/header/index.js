49860c856a03eecf6f8e79efaf224aa60b2977a3
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-02 12:10:53 +0200

    Updated to use state to switch hamburger menu icon

--- code/src/components/header/index.js
+++ code/src/components/header/index.js
@@ -4,17 +4,23 @@ import Grid from "../../lib/grid"
 import "./style.css"
 
 class Header extends React.Component {
+  state = {
+    active: true
+  }
+
+  handleToggleClassClick = () => {
+    const currentState = this.state.active
+    this.setState({
+      active: !currentState
+    })
+  }
 
   render() {
-    const toggleChange = () => {
-      document.getElementById("hamburger-container").classList.toggle("change")
-    }
-
     return (
       <header>
         <Grid className="header-grid">
           <label htmlFor="hamburger-trigger" className="hamburger-label">
-            <div id="hamburger-container" className="hamburger-container" onClick={toggleChange} onKeyPress={toggleChange} role="button" tabIndex={0}>
+            <div className={this.state.active ? "hamburger-container" : "hamburger-container change"} onClick={this.handleToggleClassClick} onKeyPress={this.handleToggleClassClick} role="button" tabIndex="0">
               <div className="hamburger bar1" />
               <div className="hamburger bar2" />
               <div className="hamburger bar3" />
