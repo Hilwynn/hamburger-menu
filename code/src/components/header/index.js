246622ab46fd2b81bb4c928adbba2e3eac2be61b
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-16 09:52:07 +0200

    Changed change class name to active

--- code/src/components/header/index.js
+++ code/src/components/header/index.js
@@ -5,7 +5,7 @@ import "./style.css"
 
 class Header extends React.Component {
   state = {
-    active: true
+    active: false
   }
 
   handleToggleClassClick = () => {
@@ -16,18 +16,22 @@ class Header extends React.Component {
   }
 
   render() {
+    // let hamburgerClass = "hamburger-container"
+    // if (this.state.active) {
+    //   hamburgerClass = "hamburger-container active"
+    // } // This Would give us a variable to use instead of the ternery operator
+
     return (
       <header>
         <Grid className="header-grid">
           <label htmlFor="hamburger-trigger" className="hamburger-label">
-            <div className={this.state.active ? "hamburger-container" : "hamburger-container change"} onClick={this.handleToggleClassClick} onKeyPress={this.handleToggleClassClick} role="button" tabIndex="0">
+            <div className={this.state.active ? "hamburger-container active" : "hamburger-container"} onClick={this.handleToggleClassClick} onKeyPress={this.handleToggleClassClick} role="button" tabIndex="0">
               <div className="hamburger bar1" />
               <div className="hamburger bar2" />
               <div className="hamburger bar3" />
             </div>
           </label>
-          <input id="hamburger-trigger" className="hamburger-checkbox" type="checkbox" />
-          <div className="navigation">
+          <div className={this.state.active ? "navigation active" : "navigation"}>
             <a href="http://technigo.io">Technigo</a>
             <a href="http://technigo.io">Boot Camp</a>
             <a href="http://technigo.io">Stories</a>
